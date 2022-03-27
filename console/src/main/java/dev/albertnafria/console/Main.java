6e43a9f40565db84650d3056b02dafc5194c2e76
Albert Nafría <albert.nafria@gmail.com>
2022-03-27 09:24:56 +0200

    Console module setup + ApplicationListener

--- console/src/main/java/dev/albertnafria/console/Main.java
+++ console/src/main/java/dev/albertnafria/console/Main.java
@@ -1,10 +1,10 @@
-package dev.albertnafria;
+package dev.albertnafria.console;
 
+import dev.albertnafria.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
