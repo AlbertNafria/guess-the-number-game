62ea222b8395caa61d6b0beae2e125ed6b1e9b2d
Albert Nafría <albert.nafria@gmail.com>
2023-04-01 20:53:07 +0200

    Add minNumber functionality

--- core/src/main/java/dev/albertnafria/config/GameConfig.java
+++ core/src/main/java/dev/albertnafria/config/GameConfig.java
@@ -2,21 +2,34 @@ package dev.albertnafria.config;
 
 import dev.albertnafria.GuessCount;
 import dev.albertnafria.MaxNumber;
+import dev.albertnafria.MinNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // Fields
-    private int maxNumber = 50;
-    private int guessCount = 8;
+    @Value("${game.maxNumber:50}") //50 default
+    private int maxNumber;
+    @Value("${game.minNumber:0}") //0 default
+    private int minNumber;
+    @Value("${game.guessCount:5}") //5 default
+    private int guessCount;
 
     // Bean Methods
     @Bean
     @MaxNumber
     public int maxNumber() {
         return maxNumber;
+    }
+    @Bean
+    @MinNumber
+    public int minNumber() {
+        return minNumber;
     }
     @Bean
     @GuessCount
